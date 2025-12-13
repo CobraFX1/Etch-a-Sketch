fbd005cf3e91217af395833513c4775dab72add7
Jacob Stephen <cashflow34steph@gmail.com>
2025-12-13 21:52:14 +0100

    Added a styles for css with the box class

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * first create a div container
+ * create all the divs inside it 
+ * put all the divs to flex
+ * adjust the box so that the divs are 16 per row 
+ * and they are 16 rows
+ * 
+ * create a class 
+ * let the class has the properties of a square.
+ */
+
+let divContainer = document.createElement("div");
+for (let i = 1; i <= (16*16); i++){
+    let
+}
