b07304afcb289231e308ee5b67127e528d1d7f86
Jacob Stephen <Cashflow34steph@gmail.com>
2025-12-15 19:09:49 +0100

    Completed the resizing
    added an event listener that converts number inouted to grids
    used a lot of effort but i am finally done.

--- script.js
+++ script.js
@@ -16,21 +16,48 @@
  * check if the clicked element has the class
  */
 let divContainer = document.querySelector("div");
-divContainer.classList.add("container");
+let numberOfSquares;
 let button = document.createElement("button");
+let newDiv;
+
+divContainer.classList.add("container");
 button.textContent = "Resize number of squares";
 document.body.prepend(button);
-let newDiv;
 for (let i = 1; i <= 256; i++) {
   newDiv = document.createElement("div");
-  newDiv.classList.add("box");
+  newDiv.classList.add("initial");
   divContainer.appendChild(newDiv);
 }
 document.addEventListener("mouseover", function (event) {
-  if (event.target.classList.contains("box")) {
+  if (
+    event.target.classList.contains("initial") ||
+    event.target.classList.contains("box")
+  ) {
     event.target.style.backgroundColor = "black";
   }
 });
+button.addEventListener("click", () => {
+  numberOfSquaresPrompt();
+  if (numberOfSquares > 100) {
+    alert("The maximun number should be 100");
+    numberOfSquaresPrompt();
+  }
+  divContainer.textContent = "";
+  for (let i = 1; i <= numberOfSquares * numberOfSquares; i++) {
+    newDiv = document.createElement("div");
+    newDiv.classList.add("box");
+    sizeOfBox = 400 / numberOfSquares;
+    newDiv.style.cssText = `
+    width: ${sizeOfBox}px; 
+    height: ${sizeOfBox}px;
+    `;
+
+    divContainer.appendChild(newDiv);
+  }
+});
+function numberOfSquaresPrompt() {
+  numberOfSquares = prompt("Enter the size of grid that you want ", "e.g 10");
+}
 /**
  * Next step
  * add a button at the top of the screen
@@ -39,4 +66,19 @@ document.addEventListener("mouseover", function (event) {
  * nw grid should be generated in the same spac
  * max should be 100
  * the elements should somehow resize
+ */
+
+/**
+ * The last step is the hardest now
+ * I need to let the elemnts resixe in that same space
+ * for that I need to get the width of the container
+ * then divide it by the number of squares entered
+ * then set the width and height of each box to that value
+ *
+ * i need to remove the divContainer while appending a new one
+ * i should do that while referrencing the same div in the html
+ * then append the new boxes to it
+ *
+ * switch that i would rather empty the container instead
+ *
  */
