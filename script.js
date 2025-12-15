d5ebbbbc4a96f51a605c9a16f56d4c9ad590adf9
Jacob Stephen <Cashflow34steph@gmail.com>
2025-12-15 17:55:29 +0100

    Completed step 3 by adding an hover effect
    An event listener was added so the boxes leave a pixelated view

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 /**
  * first create a div container
- * create all the divs inside it 
+ * create all the divs inside it
  * put all the divs to flex
- * adjust the box so that the divs are 16 per row 
+ * adjust the box so that the divs are 16 per row
  * and they are 16 rows
- * 
- * create a class 
- * let the class has the properties of a
+ *
+ * create a class
+ * let the class has the properties of a box
+ *
+ * For the hover effect
+ * create an event listener for mouseover
+ * change the background color of the box to black
+ *
+ * add an event listener to the whole document
+ * check if the clicked element has the class
  */
 let divContainer = document.querySelector("div");
 divContainer.classList.add("container");
-for (let i = 1; i <= 256; i++){
-    let newdiv = document.createElement("div");
-    newdiv.classList.add("box");
-    divContainer.appendChild(newdiv);
+let newDiv;
+for (let i = 1; i <= 256; i++) {
+  newDiv = document.createElement("div");
+  newDiv.classList.add("box");
+  divContainer.appendChild(newDiv);
 }
-divContainer.addEventListener('mouseover', () => {
-    newD
-})
+document.addEventListener("mouseover", function (event) {
+  if (event.target.classList.contains("box")) {
+    event.target.style.backgroundColor = "black";
+  }
+});
+document.addEventListener("mouseout", function (event) {
+  if (event.target.classList.contains("box")) {
+    event.target.style.backgroundColor = "white";
+  }
+});
